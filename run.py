7755099a940b2fb97809433987758fbbb37f0629
NekoDerek <senapiguru1488@gmail.com>
2020-06-16 14:37:22 +0300

    fill paragraph

--- run.py
+++ run.py
@@ -42,7 +42,8 @@ def send_data_by_country():
             url,
             data=None,
             headers={
-                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) "
+                              "Chrome/35.0.1916.47 Safari/537.36 "
             }
         )
         f = urllib.request.urlopen(req)
